12ba3847ecdb308fc530a40425aa8db62651b43b
Ritesh Kumar <riteshkr198312@gmail.com>
2013-10-18 11:41:34 +0530

    Update rails4-bootstrap.rb

--- rails4-bootstrap.rb
+++ rails4-bootstrap.rb
@@ -54,7 +54,7 @@ end
 gem "bootstrap-sass", "~> 2.3.2.1"
 gem "font-awesome-sass-rails"
 gem "simple_form"
-gem "devise", "~> 3.0.0"
+gem "devise"
 gem "cancan"
 gem "omniauth"
 gem "omniauth-facebook"
